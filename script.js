10ce2cf78812be736500a741fd47d417d4f70804
Gavin King <gavin@vimeo.com>
2017-02-06 12:44:44 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() 
+{
+	$('#template').bind('input propertychange', function() 
+	{
+		updateBookmark();
+	});
+
+	updateBookmark()
+});
+
+function updateBookmark()
+{
+	var bookmark = $('#bookmark')
+
+	templateText = $('#template').val().replace(/\r?\n/g, '\\n'); // Preserve the line formatting
+
+	if (templateText && templateText != "")
+	{
+		var href = "javascript:(function(){ var textarea = document.getElementById('pull_request_body'); textarea.value = '" + templateText + "'; })();";
+
+		bookmark.attr('href', href);
+
+		bookmark.css('opacity', '1');
+	}
+	else
+	{
+		bookmark.attr('href', null);
+
+		bookmark.css('opacity', '0.5');
+	}
+}
