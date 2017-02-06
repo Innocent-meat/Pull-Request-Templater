879a643d3b5a9d3b9ea472aaf4fa5979ded0cae5
Gavin King <gavin@vimeo.com>
2017-02-06 14:42:44 -0500

    indentation

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
 $( document ).ready(function()
 {
-	$('#template').bind('input propertychange', function()
-	{
-		updateBookmark();
-	});
+    $('#template').bind('input propertychange', function()
+    {
+        updateBookmark();
+    });
 
-	updateBookmark();
+    updateBookmark();
 });
 
 function updateBookmark()
 {
-	var bookmark = $('#bookmark');
+    var bookmark = $('#bookmark');
 
-	var templateText = $('#template').val().replace(/\r?\n/g, '\\n'); // Preserve the line formatting
+    var templateText = $('#template').val().replace(/\r?\n/g, '\\n'); // Preserve the line formatting
 
-	if (templateText && templateText != "")
-	{
-		var href = "javascript:(function(){ var textarea = document.getElementById('pull_request_body'); textarea.value = '" + templateText + "'; })();";
+    if (templateText && templateText != "")
+    {
+        var href = "javascript:(function(){ var textarea = document.getElementById('pull_request_body'); textarea.value = '" + templateText + "'; })();";
 
-		bookmark.attr('href', href);
+        bookmark.attr('href', href);
 
-		bookmark.css('opacity', '1');
-	}
-	else
-	{
-		bookmark.attr('href', null);
+        bookmark.css('opacity', '1');
+    }
+    else
+    {
+        bookmark.attr('href', null);
 
-		bookmark.css('opacity', '0.5');
-	}
+        bookmark.css('opacity', '0.5');
+    }
 }
